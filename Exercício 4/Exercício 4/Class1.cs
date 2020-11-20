7e586dbccc6d5da025a1d1615763bce9eba5f6d0
kaiquec3 <74798358+kaiquec3@users.noreply.github.com>
2020-11-20 20:12:11 -0300

    Add files via upload

---
+++ Exercício 4/Exercício 4/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_4
+{
+    class Triangulo
+    {
+        double @base;
+        double altura;
+        double area;
+
+        public void SetBase(double p)
+        {
+            @base = p; 
+        }
+
+        public void SetAltura(double p)
+        {
+            altura = p;
+        }
+
+        public double GetBase()
+        {
+            return @base;
+        }
+
+        public double GetAltura()
+        {
+            return altura;
+        }
+
+        public double GetArea()
+        {
+            return area;
+        }
+
+        public double CalcularArea()
+        {
+            area = (@base * altura) / 2;
+            return area;
+        }
+    }
+}
---
+++ Exercício 4/Exercício 4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercício_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new Triangulo();
+            double k;
+
+            do
+            {
+                Console.Write("Informe o valor da base do triângulo: ");
+                k = double.Parse(Console.ReadLine());
+                if (k > 0)
+                {
+                    a1.SetBase(k);
+                }
+            }
+            while (k <= 0);
+
+            Console.Write("Informe o valor da altura do triângulo: ");
+            a1.SetAltura(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"O valor da área do triângulo é igual a {a1.CalcularArea()}");
+        }
+    }
+}
